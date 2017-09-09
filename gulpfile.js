09e825c36b88d5f4bf55289de18139bd237f7b3c
Alejandro Martinez^2 <alejandro.martinez@metalab.csun.edu>
2017-09-09 10:05:27 -0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+var argv = require('yargs').argv;
+var bin = require('./tasks/bin');
+
+elixir.config.assetsPath = 'source/_assets';
+elixir.config.publicPath = 'source';
+
+elixir(function(mix) {
+    var env = argv.e || argv.env || 'local';
+    var port = argv.p || argv.port || 3000;
+
+    mix.copy('source/_assets/js','source/js')
+    .copy('source/_assets/fonts','source/fonts')
+    .copy('source/_assets/img','source/img')
+    .copy('source/_assets/css/bootstrap.min.css','source/css')
+    .copy('source/_assets/css/demo.css','source/css')
+    .sass('now-ui-kit.scss')
+        .exec(bin.path() + ' build ' + env, ['./source/*', './source/**/*', '!./source/_assets/**/*'])
+        .browserSync({
+            port: port,
+            server: { baseDir: 'build_' + env },
+            proxy: null,
+            files: [ 'build_' + env + '/**/*' ]
+        });
+
+});
+
---
+++ source/_layouts/master.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Now Ui Kit by Creative Tim</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    <!-- CSS Files -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/now-ui-kit.css?v=1.1.0" rel="stylesheet" />
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="./assets/css/demo.css" rel="stylesheet" />
+</head>
+    <body class="index-page sidebar-collapse">
+        @yield('body')
+    </body>
+<script src="./js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="./js/core/popper.min.js" type="text/javascript"></script>
+<script src="./js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="./js/plugins/bootstrap-switch.js"></script>
+<script src="./js/plugins/nouislider.min.js" type="text/javascript"></script>
+<script src="./js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="./js/now-ui-kit.js?v=1.1.0" type="text/javascript"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        // the body of this function is in assets/js/now-ui-kit.js
+        nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+        if ($('.section-download').length != 0) {
+            $("html, body").animate({
+                scrollTop: $('.section-download').offset().top
+            }, 1000);
+        }
+    }
+</script>
+
+</html>
